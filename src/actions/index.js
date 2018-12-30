9d30af904ff5240a70ca47bdaa5896f798e51686
Polotenchik Koala <Polotenchik.koala@gmail.com>
2018-12-30 18:12:06 +0300

    feat: create actionTypes

---
+++ src/actions/index.js
@@ -0,0 +1,19 @@
+import { FETCH_PHONES_START, FETCH_PHONES_SUCCESS, FETCH_PHONES_FAILURE } from '../actionTypes';
+
+export const fetchPhones = () => async dispatch => {
+  dispatch({ type: FETCH_PHONES_START })
+
+  try { 
+    const phones = await fetchPhonesApi() 
+    dispatch({
+      type: FETCH_PHONES_SUCCESS,
+      payload: phones
+    }) 
+  } catch (err) {
+    dispatch({
+      type: FETCH_PHONES_FAILURE,
+      payload: err,
+      error: true
+    })
+  }
+}
--- src/containers/phones.js
+++ src/containers/phones.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-const Phones = () => (
-    <div>
+class Phones extends Component {
+
+  componentDidMount() {
+    this.props.fetchPhones()
+  }
+  
+  render() {
+    return (
+      <div>
         Phones
-    </div>
-)
+      </div>
+    )
+  }
+}
 
-export default Phones;
+const mapDispatchToProps = {
+  fetchPhones
+}
+
+export default connect(null, mapDispatchToProps)(Phones);
