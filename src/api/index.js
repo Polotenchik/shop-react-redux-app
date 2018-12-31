97940997921604c1279a6dea1cb65415d584ffab
Polotenchik Koala <Polotenchik.koala@gmail.com>
2018-12-31 20:23:29 +0300

    feat: prepare data for phonePage

--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,5 @@
 import phones from './mockPhones';
+import R from 'ramda';
 
 export const fetchPhones = async () => {
     return new Promise(resolve => {
@@ -10,4 +11,11 @@ export const loadMorePhones = async ({ offset }) => {
     return new Promise(resolve => {
         resolve(phones)
     })
+}
+
+export const fetchPhoneById = async id => {
+    return new Promise(resolve => {
+        const phone = R.find(R.propEq('id',id), phones);
+        resolve(phone);
+    });
 }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,9 +3,11 @@ import { routerReducer } from 'react-router-redux';
 
 import phones from './phones';
 import phonePage from './phonePage';
+import devicePage from './devicePage'
 
 export default combineReducers({
     routing: routerReducer,
     phones,
-    phonePage
+    phonePage,
+    devicePage
 })
