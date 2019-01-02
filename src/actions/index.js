10a572e6ef4792babbb3770a3186e4b57947a96f
Polotenchik Koala <Polotenchik.koala@gmail.com>
2019-01-02 16:40:11 +0300

    feat: add Search component

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,7 +2,7 @@ import {
   FETCH_PHONES_START, FETCH_PHONES_SUCCESS, FETCH_PHONES_FAILURE,
   LOAD_MORE_PHONES_START, LOAD_MORE_PHONES_SUCCESS, LOAD_MORE_PHONES_FAILURE,
   FETCH_PHONE_BY_ID_START, FETCH_PHONE_BY_ID_SUCCESS, FETCH_PHONE_BY_ID_FAILURE,
-  ADD_PHONE_TO_BASKET
+  ADD_PHONE_TO_BASKET, SEARCH_PHONE
 } from '../actionTypes';
 import { getRenderedPhonesLength } from '../selectors';
 
@@ -71,5 +71,12 @@ export const addPhoneToBasket = id => dispatch => {
   dispatch({
     type: ADD_PHONE_TO_BASKET,
     payload: id
+  });
+}
+
+export const searchPhone = text => dispatch => {
+  dispatch({
+    type: SEARCH_PHONE,
+    payload: text
   });
 }
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import BasketCart from './BasketCard';
-
+import Search from './Search';
 
 const Sidebar = () => (
     <div>
         <BasketCart />
+        <Search />
     </div>
 );
 
